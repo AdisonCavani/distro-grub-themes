f25d12a894754cf1951cd198298942a6463b0b32
Adison Cavani <adrian.srodon@gmail.com>
2021-10-09 18:45:08 +0200

    Add new auto-update C# program

---
+++ src/ProgramOptions.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace DistroGrubThemes
+{
+    public class ProgramOptions
+    {
+        [Option('a', "archive", HelpText = "Create theme archive.")]
+        public string ArchivedFiles { get; set; }
+    }
+}
